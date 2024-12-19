f91f62fd97530d76a96dc883aa9a2ab4d1e0e250
Moises Giron <mgiron@castilloss.com>
2024-12-19 18:05:23 -0400

    Change the form to reactive form

--- src/app/routes/product/create-product/create-product.component.ts
+++ src/app/routes/product/create-product/create-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { createProductModel } from '../../../DTOS/models/product/createProductModel';
 import { productService } from '../../../services/productService';
@@ -13,6 +13,7 @@ import { createProductRequest } from '../../../DTOS/request/product/createProduc
 @Component({
   selector: 'app-create-product',
   imports: [
+    ReactiveFormsModule,
     FormsModule,
     RouterLink
   ],
@@ -33,6 +34,9 @@ export class CreateProductComponent implements OnInit{
     private router: Router;
     private route: ActivatedRoute;
 
+    // Controls of the form
+    public mainForm: FormGroup;
+
 
     constructor(
       productService: productService,
@@ -48,6 +52,7 @@ export class CreateProductComponent implements OnInit{
       this.typePaymentService = typePaymentService;
       this.router = router;
       this.route = route;
+      this.mainForm = this.getFormControls();
     }
 
 
@@ -74,4 +79,34 @@ export class CreateProductComponent implements OnInit{
       .subscribe(() => this.router.navigate(['/list-products']));
     }
 
+
+    // get the forom controls and validations
+    private getFormControls(): FormGroup {
+
+      // Validators
+      const validatorNoLetters: ValidatorFn = Validators.pattern('^\d+$');
+      const validatorNoCero: ValidatorFn = Validators.max(0);
+      
+      return new FormGroup({
+        name: new FormControl(this.model.name, [Validators.required]),
+        detail: new FormControl(this.model.detail, [Validators.required]),
+        typeCurrencyId: new FormControl(this.model.typeCurrencyId, [validatorNoCero]),
+        typePaymentId: new FormControl(this.model.typePaymentId, [validatorNoCero]),
+        implementationCost: new FormControl(this.model.implementationCost, [Validators.required, validatorNoLetters]),
+        instalationCost: new FormControl(this.model.instalationCost, [Validators.required, validatorNoLetters]),
+        regularPrice: new FormControl(this.model.regularPrice, [Validators.required, validatorNoLetters]),
+        advancePrice: new FormControl(this.model.advancePrice, [Validators.required, validatorNoLetters])
+      });
+    }
+
+
+    // Getters of controls form
+    get name() { return this.mainForm.get('name')!; }
+    get detail() { return this.mainForm.get('detail')!; }
+    get typeCurrencyId() { return this.mainForm.get('typeCurrencyId')!; }
+    get typePaymentId() { return this.mainForm.get('typePaymentId')!; }
+    get implementationCost() { return this.mainForm.get('implementationCost')!; }
+    get instalationCost() { return this.mainForm.get('instalationCost')!; }
+    get regularPrice() { return this.mainForm.get('regularPrice')!; }
+    get advancePrice() { return this.mainForm.get('advancePrice')!; }
 }
